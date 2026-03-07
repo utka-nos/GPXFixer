00593b276d6e7248194f9e11920afe983848faf2
Dmitry Kolesnikov <dkolesnikov@ultimeta.ru>
2026-03-08 01:25:13 +0400

    fix ios app's problems

--- iosApp/GPXFixer/GPXFixer/ComposeView.swift
+++ iosApp/GPXFixer/GPXFixer/ComposeView.swift
@@ -4,7 +4,7 @@ import shared
 
 struct ComposeView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
-        MainViewControllerKt.MainViewController()
+        ComposeEntryPoint().makeViewController()
     }
 
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
--- shared/src/iosMain/kotlin/com/gpxeditor/shared/MainViewController.kt
+++ shared/src/iosMain/kotlin/com/gpxeditor/shared/MainViewController.kt
@@ -2,4 +2,10 @@ package com.gpxeditor.shared
 
 import androidx.compose.ui.window.ComposeUIViewController
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController(
+    configure = { enforceStrictPlistSanityCheck = false }
+) { App() }
+
+class ComposeEntryPoint {
+    fun makeViewController() = MainViewController()
+}
